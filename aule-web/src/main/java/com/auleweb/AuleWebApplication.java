f4f83d3399892806567e6767f1e004e92719b7a2
Daniele Cannelonga <57572295+DanieleCannelonga@users.noreply.github.com>
2023-07-26 12:57:11 +0200

    Add files via upload

---
+++ aule-web/src/main/java/com/auleweb/AuleWebApplication.java
@@ -0,0 +1,13 @@
+package com.auleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuleWebApplication.class, args);
+	}
+
+}
